b41e8f54064d5645437198562d1caa7f4eea3f7f
Anant Kanchan <66024577+anantcodes@users.noreply.github.com>
2022-05-11 07:19:42 +0530

    Update bestFirstSearch.py

--- bestFirstSearch.py
+++ bestFirstSearch.py
@@ -34,3 +34,11 @@ source = int(input("Enter the Source Node: "))
 target = int(input("Enter the Target/Destination Node: ")) 
 print("\nPath: ", end = "") 
 best_first_search(source, target, v)
+
+# Step 1: Place the starting node into the OPEN list.
+# Step 2: If the OPEN list is empty, Stop and return failure.
+# Step 3: Remove the node n, from the OPEN list which has the lowest value of h(n), and places it in the CLOSED list.
+# Step 4: Expand the node n, and generate the successors of node n.
+# Step 5: Check each successor of node n, and find whether any node is a goal node or not. If any successor node is goal node, then return success and terminate the search, else proceed to Step 6.
+# Step 6: For each successor node, algorithm checks for evaluation function f(n), and then check if the node has been in either OPEN or CLOSED list. If the node has not been in both list, then add it to the OPEN list.
+# Step 7: Return to Step 2.
